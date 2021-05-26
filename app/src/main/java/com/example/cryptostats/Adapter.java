b9a5d7ebd7f013a1fb7434a8bfdb5ea6afd51840
Shubham Mane <shubhammane434@gmail.com>
2021-05-27 00:11:30 +0530

    App Connected to Api and created main crypto-list view.

---
+++ app/src/main/java/com/example/cryptostats/Adapter.java
@@ -0,0 +1,155 @@
+package com.example.cryptostats;
+
+
+import android.content.Context;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.model.StreamEncoder;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou;
+import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYouListener;
+import com.github.twocoffeesoneteam.glidetovectoryou.SvgDecoder;
+import com.github.twocoffeesoneteam.glidetovectoryou.SvgDrawableTranscoder;
+import com.github.twocoffeesoneteam.glidetovectoryou.SvgSoftwareLayerSetter;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implements Filterable {
+
+    LayoutInflater mInflater;
+    Context context;
+    List<DataModel> dm;
+    List<DataModel> dmfilter;
+
+    public Adapter(Context context, List<DataModel> dm) {
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.dm = dm;
+        this.dmfilter=new ArrayList<>(dm);
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v ;
+        LayoutInflater layoutInflater =  LayoutInflater.from(context);
+        v = layoutInflater.inflate(R.layout.itemsview,parent,false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+
+
+
+        holder.currency.setText(dmfilter.get(position).getCurrency());
+        holder.name.setText(dmfilter.get(position).getName());
+        holder.price.setText(dmfilter.get(position).getPrice()+" ₹");
+
+
+        RequestBuilder<PictureDrawable> requestBuilder = GlideToVectorYou
+                .init()
+                .with(context)
+                .getRequestBuilder();
+
+        requestBuilder
+                .load(dmfilter.get(position).getLogo_url())
+                .apply(new RequestOptions().override(100,100))
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
+                .into(holder.logo_url);
+
+//.transition(DrawableTransitionOptions.withCrossFade())
+
+        Glide.with(context)
+                .load(dmfilter.get(position).getLogo_url())
+                .apply(new RequestOptions().override(100, 100))
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(50)))
+                .into(holder.logo_url);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dmfilter.size();
+
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                String character = constraint.toString();
+                if(character.isEmpty()){
+                    dmfilter = dm;
+                }
+
+                else {
+                    List<DataModel> filterlist = new ArrayList<>();
+
+                    for (DataModel dataModel :dm){
+                        if(dataModel.getName().toLowerCase().contains(character.toLowerCase())){
+                            filterlist.add(dataModel);
+                        }
+
+                    }
+
+                    dmfilter = filterlist;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = dmfilter;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+
+                dmfilter = (ArrayList<DataModel>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+
+    public  static  class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView currency;
+        ImageView logo_url;
+        TextView name;
+        TextView price ;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            currency = itemView.findViewById(R.id.name);
+            logo_url = itemView.findViewById(R.id.logo);
+            name = itemView.findViewById(R.id.full_name);
+            price = itemView.findViewById(R.id.price);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cryptostats/DataModel.java
@@ -0,0 +1,27 @@
+package com.example.cryptostats;
+
+public class DataModel {
+
+    private String currency;
+    private String name;
+    private String logo_url;
+    private float price;
+
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogo_url() {
+        return logo_url;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
+
