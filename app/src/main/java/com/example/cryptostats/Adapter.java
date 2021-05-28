d4e56778c644ae94af4da0628d3dd1fe835c5732
Shubham Mane <shubhammane434@gmail.com>
2021-05-29 02:03:27 +0530

    addded newscard

--- app/src/main/java/com/example/cryptostats/Adapter.java
+++ app/src/main/java/com/example/cryptostats/Adapter.java
@@ -14,7 +14,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
---
+++ app/src/main/java/com/example/cryptostats/NewsActivity.java
@@ -0,0 +1,17 @@
+package com.example.cryptostats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+
+
+
+    }
+}
